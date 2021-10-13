51bc7b25f25dacc97eecc104b04132147dc471bd
alxnull <alxnull@e.mail.de>
2021-10-13 12:53:30 +0200

    UpdateNotifyMode comments

--- AppHelpers.WPF/Update/UpdateNotifyMode.cs
+++ AppHelpers.WPF/Update/UpdateNotifyMode.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 
-#pragma warning disable 1591
-
 namespace Bluegrams.Application
 {
     /// <summary>
@@ -9,10 +7,25 @@ namespace Bluegrams.Application
     /// </summary>
     public enum UpdateNotifyMode
     {
+        /// <summary>
+        /// Show a notification if a new update is available.
+        /// </summary>
         NewUpdate = 0,
+        /// <summary>
+        /// Always show a notification.
+        /// </summary>
         Always = 1,
+        /// <summary>
+        /// Show a notification if a new update is available that has not been shown to the user before.
+        /// </summary>
         NewUncheckedUpdate = 2,
+        /// <summary>
+        /// Never show a notification.
+        /// </summary>
         Never = 3,
+        /// <summary>
+        /// Show a notification if a new update is available and allow the user to skip notifications for the new version.
+        /// </summary>
         Auto = 4
     }
 }

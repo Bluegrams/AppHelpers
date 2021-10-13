2aca910f818386185ae26e63ae8dfc2dcbbc1286
alxnull <alxnull@e.mail.de>
2021-10-13 11:36:34 +0200

    Bump to v.0.1.0

--- AppHelpers.WinForms/Properties/AssemblyInfo.cs
+++ AppHelpers.WinForms/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bluegrams")]
 [assembly: AssemblyProduct("AppHelpers.WinForms")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2019-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.11")]
-[assembly: AssemblyFileVersion("0.1.0.11")]
+[assembly: AssemblyVersion("0.1.0.12")]
+[assembly: AssemblyFileVersion("0.1.0.12")]
